d1eddba87f5427738914bff2ac702cf3bb70c72a
kimura <37994567+koji-kimura@users.noreply.github.com>
2019-02-27 08:24:52 +0900

    本日勉強分

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
+import { Search } from "./components/Search";
+
+import "./App.css";
+
 class App extends React.Component {
   constructor() {
     super();
@@ -10,9 +14,13 @@ class App extends React.Component {
 
   renderImageList(list) {
     const imageList = list.map(url => {
-      return <img src={url} />;
+      return (
+        <li className="item">
+          <img className="image" src={url} />
+        </li>
+      );
     });
-    return imageList;
+    return <ul className="list">{imageList}</ul>;
   }
   // ライフサイクルメソッドがある
   componentDidMount() {
@@ -20,13 +28,18 @@ class App extends React.Component {
   }
   render() {
     // console.log(this.state.gifUrlList);
-    return <div>{this.renderImageList(this.state.gifurlList)}</div>;
+    return (
+      <div>
+        <Search search={this.giphyApi} />
+        {this.renderImageList(this.state.gifUrlList)}
+      </div>
+    );
   }
 
-  giphyApi() {
-    const search = "cat";
+  giphyApi = target => {
+    const search = target;
     const key = "cSY7uF4g6yCC7gx8ls4EJS7GBkEBhOKq";
-    const limit = 3;
+    const limit = 10;
 
     const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`;
 
@@ -36,7 +49,7 @@ class App extends React.Component {
       const imageUrlList = data.map(item => item.images.downsized.url);
       this.setState({ gifUrlList: imageUrlList });
     });
-  }
+  };
 }
 
 // function App() {
